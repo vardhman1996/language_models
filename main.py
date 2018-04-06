e9fa174c473b92ce886f25d69f80b6d673e5f733
Vardhman Mehta <mehtav@cs.washington.edu>
2018-04-05 17:06:47 -0700

    data files

--- main.py
+++ main.py
@@ -1,1 +1,16 @@
-print("Hello")
+import nltk
+from nltk import word_tokenize
+
+
+s = "ওয়ান পিছের তুমি কতো গুল পর্ব দেখেছো?"
+l = [i for i in s]
+p = s.encode("utf-8")
+print(l[0])
+
+
+v = b'\x75'.decode("utf-8")
+# print(v)
+
+
+print(ord(l[0]))
+print(chr(2451))
